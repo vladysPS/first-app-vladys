fce82a6a1bbc36061ed727f8b7766f4fcd109d4f
vladysPS <vladyslavk@pangostudio.com>
2026-08-17 16:15:39 +0200

    added new admin page with table of all tickets

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -18,9 +18,8 @@ export default function App() {
   return (
     <AppProvider embedded apiKey={apiKey}>
       <s-app-nav>
-        <s-link href="/app">Home</s-link>
-        <s-link href="/app/additional">Additional page</s-link>
         <s-link href="/app/contactPartner">Contact Partner</s-link>
+        <s-link href="/app/admin">Admin</s-link>
       </s-app-nav>
       <Outlet />
     </AppProvider>
